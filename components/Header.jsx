9ffc2bd706bf7ef5c81ae7b1663860f6731020ad
Nikhil Patel <Nikhil.patel2002@hotmail.com>
2025-02-12 18:17:06 +0000

    Header Adjustment

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import { SocialIcon } from 'react-social-icons';
 
 export default function Header() {
   return (
-    <header className='sticky top-0 p-5 flex items-start justify-between max-w-6xl mx-auto z-20 xl:items-center'>
+    <header className=' top-0 p-5 flex items-start justify-between max-w-6xl mx-auto z-20 xl:items-center'>
       <motion.div
         initial={{
           x: -500,
