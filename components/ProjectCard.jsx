09545be148d876477e9a6b4a0be7c1fbbdb940a2
Nikhil Patel <Nikhil.patel2002@hotmail.com>
2026-06-14 15:27:01 +0100

    Project Card size Fixed

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -83,26 +83,28 @@ const ProjectCard = ({ imgUrl, tags, title, description, githubUrl, demoUrl }) =
 
   return (
     <>
-      <article className="flex flex-col rounded-lg items-center justify-evenly flex-shrink-0 bg-[#292929] p-6 overflow-hidden snap-center 
-      md:w-[250px] md:h-[400px] w-[290px] h-[450px] border border-red-300 shadow-dark-red">
+      <article className="flex flex-col rounded-lg items-center flex-shrink-0 bg-[#292929] p-5 md:p-6 overflow-hidden snap-center 
+      md:w-[270px] md:h-[470px] w-[290px] h-[480px] border border-red-300 shadow-dark-red">
         <img
-          className="w-32 h-32 xl:w-[150px] xl:h-[150px] object-contain object-center"
+          className="w-28 h-28 md:w-32 md:h-32 xl:w-36 xl:h-36 object-contain object-center"
           src={imgUrl}
           alt={title}
         />
-        <div className="flex space-x-2 mt-4">
+        <div className="flex flex-wrap justify-center gap-2 mt-4 min-h-[52px]">
           {tags.map((tag, index) => (
-            <span key={index} className="px-2 py-1 bg-[#f07171] bg-opacity-10 text-[#ff2f2f] rounded-full text-xs">
+            <span key={index} className="px-2 py-1 bg-[#f07171] bg-opacity-10 text-[#ff2f2f] rounded-full text-[11px] whitespace-nowrap">
               {tag}
             </span>
           ))}
         </div>
-        <div className="text-left w-full mt-4">
+        <div className="flex flex-1 flex-col text-left w-full mt-4">
           <h4 className="text-xl font-semibold mb-2">{title}</h4>
-          <div className="max-w-[250px] mb-4 min-h-[64px]">
-            <p className="text-gray-300 text-sm">{description}</p>
+          <div className="max-w-[250px] min-h-[118px]">
+            <p className="text-gray-300 text-sm leading-relaxed overflow-hidden [display:-webkit-box] [-webkit-line-clamp:5] [-webkit-box-orient:vertical]">
+              {description}
+            </p>
           </div>
-          <div className="flex justify-center space-x-4 px-10 py-7">
+          <div className="mt-auto flex justify-center gap-3 px-0 pt-5">
             <StyledButton href={githubUrl} target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faCode} className="mr-2" />
               <span>Code</span>
